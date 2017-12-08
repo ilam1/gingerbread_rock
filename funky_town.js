9608b4f07c9315bc3701318c257953d557d91f13
Irene Lam <ilam1@stuy.edu>
2017-12-07 22:56:53 -0500

    Buttons

--- funky_town.js
+++ funky_town.js
@@ -1,4 +1,15 @@
+/*
+Gingerbread Rock
+Adam Abbas and Irene Lam
+SoftDev1 pd7
+HW#15 -- Sequential Progression
+2017-12-07
+*/
+
+//Fibonacci
 var fibonacci = function(n){
+    if (n == 0)
+        return 0;
     if(n <= 2){
         return 1;
     }
@@ -21,6 +32,7 @@ console.log("n = 5");
 console.log(fibonacci(5));
 console.log("Should be 5");
 
+//GCD
 var gcd = function(a,b){
   //Shoutout to Arif for being a real homie.
     var t;
@@ -46,8 +58,10 @@ console.log("A = 24, B = 36");
 console.log(gcd(24, 36));
 console.log("Should be 12");
 
+//List of students
 var students = ["student1", "student2", "student3", "student4", "student5", "student6", "student7", "student8"];
 
+//Random student from above list
 var randomStudent = function(){
     var len_stud = students.length;
     var rand_ind = Math.floor(Math.random() * len_stud);
@@ -63,3 +77,33 @@ console.log(randomStudent());
 console.log(randomStudent());
 console.log('\n\n\n\n\n\n\n\n')
 console.log("Button testings");
+
+//Buttons for...
+//Fibonacci (random number tested between 1 and 30)
+var fibCallback = function(e) {
+    var randn = Math.ceil(Math.random()*30);
+    console.log("\n\nFibonacci call for " + randn.toString());
+    console.log(fibonacci(randn));
+}
+var fib = document.getElementById("fib");
+fib.addEventListener("click", fibCallback);
+
+//GCD
+var gcdCallback = function(e) {
+    var randa = Math.ceil(Math.random()*1000);
+    var randb = Math.ceil(Math.random()*50);
+    console.log("\n\nGCD call for " + randa.toString() + " and " + randb.toString());
+    console.log(gcd(randa, randb));
+}
+var gcd1 = document.getElementById("gcd1");
+gcd1.addEventListener("click", gcdCallback);
+
+//Random Student
+var randstudentCallback = function(e) {
+    console.log("\n\nHere are 3 random students (out of 8):");
+    console.log(randomStudent());
+    console.log(randomStudent());
+    console.log(randomStudent());
+}
+var randstudent = document.getElementById("randstudent");
+randstudent.addEventListener("click", randstudentCallback);
